caebd70e7680a83ba42634df6ec335e03a24bce0
adriaanbalt <adriaan@balt.us>
2015-10-09 15:41:33 -0400

    details page updated by removing angular references from angular variation

--- app/assets/javascripts/pages/Details.js
+++ app/assets/javascripts/pages/Details.js
@@ -11,17 +11,18 @@ export default class Details extends Component {
 
     this.projectId = this.props.params.projectId;
 
-    this.getStateByProjectId = () => {
-      if ( AppStore.getState().projects.filter(project => project.projectId === this.projectId)[0] ) {
-        return AppStore.getState().projects.filter(project => project.projectId === this.projectId)[0];
+    this.getStateById = ( id ) => {
+      if ( AppStore.getState().projects.filter(project => project.slug === id)[0] ) {
+        return AppStore.getState().projects.filter(project => project.slug === id)[0];
       } else {
         return {};
       }
     }
 
-    this.state = this.getStateByProjectId();
-    this._onChange = () => {
-      this.setState( this.getStateByProjectId() );
+    this.state = this.getStateById( props.projectId );
+
+    this._onChange = (o) => {
+      this.setState( this.getStateById( props.projectId ) );
     }
   }
 
@@ -37,6 +38,29 @@ export default class Details extends Component {
     return (
       <div className='Details'>
         <div className='Details-container'>
+          <section>
+            <h1>{ this.state.campaign }</h1>
+            <h3></h3>
+            <p ng-repeat="paragraph in description">paragraph</p>
+            <article>
+                <div ng-repeat="block in blocks" du-parallax y="background">
+                    <p ng-if="block.copy">this.state.copy</p>
+                    <img src="block.image" ng-if="block.image || block.award"/>
+                </div>
+            </article>
+            <div class="others">
+                <div>
+                    <img src="" alt=""/>
+                    <h4>Publishing Site</h4>
+                    <p>Conde Nast</p>
+                </div>
+                <div>
+                    <img src="" alt=""/>
+                    <h4>Our Book of Liquid Gold</h4>
+                    <p>Kraft</p>
+                </div>
+            </div>
+          </section>
         </div>
       </div>
     );
