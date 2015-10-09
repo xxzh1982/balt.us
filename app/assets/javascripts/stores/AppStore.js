3e5c703f3f328a54466d65aa6a94d4dac71152ce
adriaanbalt <adriaan@balt.us>
2015-10-09 15:40:05 -0400

    appstore holds about information

--- app/assets/javascripts/stores/AppStore.js
+++ app/assets/javascripts/stores/AppStore.js
@@ -9,45 +9,33 @@ import Storage from '../lib/Storage';
  * The store is a data warehouse.                                   *
  * This is the single source of truth re: app state.                *
  * Instance vars defined anywhere in the store become the state.    *
- ********************************************************************
- ********************************************************************
- * Data should NOT be instantiated from inside of the store.        *
- * @TODO: load data asynchronously from a database.                 *
- * http://alt.js.org/guide/async/                                   *
  ********************************************************************/
 class AppStore {
   constructor () {
+    this.about = {};
     this.projects = [];
 
-    this.getProjectByProjectId = (id) => {
-      return this.projects.filter(project => project.projectId === id)[0]
-    }
-
-
     this.bindListeners({
-      loadProjects: ActionCreator.LOAD_PROJECTS,
-      projectsLoaded: ActionCreator.PROJECTS_LOADED
+      loadWebsite: ActionCreator.LOAD_WEBSITE,
+      websiteLoaded: ActionCreator.WEBSITE_LOADED
     })
 
   }
 
-  getProjectByProjectId( data ){
-    return this.getProjectByProjectId(data.projectNumber);
+  getProjectById( id ){
+    console.log ( 'getProjectById', id );
+    return this.projects.filter(project => project.projectId === id)[0]
   }
 
-  loadProjects (data) {
-    Storage.loadProjects();
-    if ( !this.interval ){
-      this.interval = setInterval(
-        () => {
-          Storage.loadProjects()
-        },
-        10000
-      );
-    }
+  loadWebsite () {
+    Storage.loadWebsite();
   }
-  projectsLoaded (data) {
+
+  websiteLoaded (data) {
     if ( data ) {
+      this.about = data.about;
+      console.log('this.about',this.about);
+      this.projects = data.projects;
     }
   }
 
