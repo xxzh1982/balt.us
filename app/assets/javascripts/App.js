216c81d716aa9e70ec9ed3ca58a252f26f79509d
adriaanbalt <adriaan@balt.us>
2015-10-18 23:25:06 -0400

    animaing between pages with CssAnimationGroupi

--- app/assets/javascripts/App.js
+++ app/assets/javascripts/App.js
@@ -4,6 +4,7 @@ import React from 'react'
 import { Router } from 'react-router'
 import ActionCreator from './actions/AppActions';
 import AppStore from './stores/AppStore';
+import ReactTransitionGroup from "react-addons-transition-group";
 
 import Hero from './components/Hero'
 import Home from './pages/Home'
@@ -12,18 +13,49 @@ const App = React.createClass({
 
   mixins: [Router.State],
 
+  componentWillAppear(callback) {
+  console.log("App componentWillAppear");
+    _animateIn(callback);
+  },
+
+  componentWillEnter(callback) {
+  console.log("App componentWillEnter");
+    _animateIn(callback);
+  },
+
+  componentWillLeave(callback) {
+  console.log("App componentWillLeave");
+    _animateOut(callback);
+  },
+
+  _animateIn(callback) {
+    console.log("_animateIn");
+    callback();
+    // var el = React.findDOMNode(this);
+    // TweenLite.set(el, {opacity: 0});
+    // setTimeout(function() {
+    //   console.log("timed in");
+    //   TweenLite.to(el, 1, {opacity: 1}).play().eventCallback("onComplete", callback);
+    // }, this.props.animateInDelay);
+  },
+
+  _animateOut(callback) {
+    console.log("_animateOut");
+    callback();
+    // var el = React.findDOMNode(this);
+    // setTimeout(function() {
+    //   TweenLite.to(el, 1, {opacity: 0}).play().eventCallback("onComplete", callback);
+    // }, this.props.animateOutDelay);
+  },
+
   render () {
-    ActionCreator.loadWebsite();
+    const { pathname } = this.props.location
     return (
-      <div className='BodyContent'>
+      <div id='main' rel="main">
         <Hero />
-        <div className='BodyContent-main'>
-          {
-            this.props.children
-              ||
-            <Home />
-          }
-        </div>
+        <ReactTransitionGroup component="div" className='content'>
+          {React.cloneElement(this.props.children || <Home />, { key: pathname })}
+        </ReactTransitionGroup>
       </div>
     );
   }
