e1de65bca2e3b059828b9dbeb3728e48a94645ca
adriaanbalt <adriaan@balt.us>
2015-10-18 23:24:38 -0400

    storage utility loads data json

--- app/assets/javascripts/lib/Storage.js
+++ app/assets/javascripts/lib/Storage.js
@@ -2,28 +2,18 @@ import jsonp from 'jsonp';
 import request from 'superagent';
 import ActionCreator from '../actions/AppActions';
 
+let loaded = false;
 export
 default {
     loadWebsite() {
+      if ( !loaded ){
+        loaded = true;
         request
           .get('/assets/data/balt.json')
           .set('Content-Type', 'application/json')
           .end(function(err, res){
             ActionCreator.websiteLoaded( JSON.parse(res.text) );
           });
-        // window.callback = (data) => {
-        //   ActionCreator.websiteLoaded(data);
-        // }
-        // jsonp('feed.js?jsonp=callback', (error, data) => {
-        //     if (error) {
-        //         console.log(`error: $ {
-        //             error.message
-        //         }`)
-        //         return error;
-        //     } else {
-        //         /* @TODO: figure out why jsonp must use the callback defined above */
-        //         ActionCreator.websiteLoaded(data);
-        //     }
-        // });
+      }
     }
 };
